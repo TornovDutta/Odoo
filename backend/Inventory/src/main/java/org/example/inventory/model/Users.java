eef9818261b20228f51182b419f724fc9d366411
TornovDutta <tornovdutta@gmail.com>
2025-11-22 09:47:21 +0530

    add the users repo

--- backend/Inventory/src/main/java/org/example/inventory/model/Users.java
+++ backend/Inventory/src/main/java/org/example/inventory/model/Users.java
@@ -1,4 +1,18 @@
 package org.example.inventory.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.lang.annotation.Documented;
+
+@Document(collection= "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Users {
+    @Id
+    private String id;
 }
